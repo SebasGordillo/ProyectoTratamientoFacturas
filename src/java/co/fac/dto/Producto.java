946289673593485e81bac3369716cbc659bdd03e
Sebas <Sebas@Sebas-Gordillo>
2020-05-13 19:43:10 -0500

    Creación dto

--- src/java/co/fac/dto/Producto.java
+++ src/java/co/fac/dto/Producto.java
@@ -6,6 +6,7 @@ package co.fac.dto;
  */
 public class Producto {
 
+    private int idProducto;
     private String nombre;
     private String marca;
     private int items;
@@ -13,10 +14,19 @@ public class Producto {
     public Producto() {
     }
 
-    public Producto(String producto, String marca, int intems) {
-        this.nombre = producto;
+    public Producto(int idProducto, String nombre, String marca, int items) {
+        this.idProducto = idProducto;
+        this.nombre = nombre;
         this.marca = marca;
-        this.items = intems;
+        this.items = items;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
     }
 
     public String getNombre() {
